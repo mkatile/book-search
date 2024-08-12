9e19032d20d8ef2d3f2c7fca53f2c653bff05613
mkatile <mariamkatile06@gmail.com>
2024-08-11 20:35:49 -0400

    update server.js file

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -26,11 +26,11 @@ const startServer = async () => {
 
   // Serve static assets if in production
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
   }
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 
   db.once('open', () => {
