e5781135801c6679986ea9bd117039212183c236
mkatile <mariamkatile06@gmail.com>
2024-08-11 22:55:38 -0400

    update mongodb

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect('mongodb://127.0.0.1:27017/googlebooks', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB connected successfully');
+    mongoose.connection.close(); // Close the connection after testing
+  })
+  .catch(err => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose.connection;
